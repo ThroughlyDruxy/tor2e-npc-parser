5f40392a8dfda555f91ca4b5153e23c72d01ba00
Adam Cottrell <31712348+ThroughlyDruxy@users.noreply.github.com>
2022-02-06 16:23:36 -0700

    Moved to scripts and put all funcitons in same file (main.js)

--- modules/main.js
+++ modules/main.js
@@ -43,8 +43,74 @@ class TOR2eParser {
   }
 }
 
-///// HOOKS /////
+///// Dialog box for input /////
+class TOR2eDialog extends FormApplication {
+  constructor(object, options) {
+    super(object, options);
+  }
 
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      template: 'modules/tor2e-npc-parser/templates/tor2e-npc-parser.hbs',
+    });
+  }
+
+  getData(options = {}) {
+    return super.getData();
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    let textArea = html.getElementbyId('text-input');
+    textArea.change(this._onTextChanged.bind(this));
+  }
+
+  async _updateObject(event, formData) {
+    return;
+  }
+}
+
+///// Creates dialog and takes data to send to tor2eParser /////
+async function textInput() {
+  console.log('textInput() called');
+
+  const html = await renderTemplate(
+    'modules/tor2e-npc-parser/templates/tor2e-npc-parser.hbs',
+    { originalText: '' }
+  );
+
+  const dialog = new Dialog({
+    title: 'TOR2e NPC Parser',
+    content: html,
+    buttons: {
+      go: {
+        icon: '<i class="fas fa-check"></i>',
+        label: 'Go',
+        callback: () => tor2eParser(originalText), // undefined here
+      },
+      cancel: {
+        icon: '<i class="fas fa-times"></i>',
+        label: 'Cancel',
+        callback: () => console.log('Chose cancel'),
+      },
+    },
+    default: 'cancel',
+    render: html => console.log('TOR2E | Dialog rendered'),
+    close: html => console.log(`TOR2E | Dialog closed.`),
+  });
+
+  dialog.render(true);
+}
+
+///// Parser /////
+function tor2eParser(strToParse) {
+  console.log(`TOR2E || tor2eParser was called`);
+  console.log(typeof strToParse);
+  console.log(strToParse);
+}
+
+///// HOOKS /////
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
   registerPackageDebugFlag(TOR2eParser.ID);
 });
--- modules/parser.js
+++ modules/parser.js
@@ -1,5 +1,1 @@
-export function tor2eParser(strToParse) {
-  console.log(`TOR2E || tor2eParser was called`);
-  console.log(typeof strToParse);
-  console.log(strToParse);
-}
+
